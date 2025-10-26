242b3815200acfa8c75123525a0c3838d23dee23
Gabriel Teixeira <aminomaster2017@gmail.com>
2025-10-25 21:01:16 -0300

    Melhorei em geral

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-const copyLinks = document.querySelectorAll('.copyable');
+const contacts = document.querySelectorAll('.contact[data-copy]');
 const toast = document.getElementById('toast');
+const toggle = document.querySelector('.theme-toggle');
 
-copyLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const textToCopy = this.dataset.copy;
-        navigator.clipboard.writeText(textToCopy).then(() => {
+contacts.forEach(c => {
+    c.addEventListener('click', () => {
+        navigator.clipboard.writeText(c.dataset.copy).then(() => {
             toast.classList.add('show');
-            setTimeout(() => toast.classList.remove('show'), 2500);
+            setTimeout(()=>toast.classList.remove('show'), 2000);
         });
     });
+});
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    toggle.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
 });
